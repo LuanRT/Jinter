8b610d1ca282065ef9822e949d3e24fb1d43152b
LuanRT <luan.lrt4@gmail.com>
2022-08-25 16:21:56 -0300

    fix: short-circuiting not working correctly

--- src/nodes/LogicalExpression.ts
+++ src/nodes/LogicalExpression.ts
@@ -4,18 +4,19 @@ export default class LogicalExpression {
   static visit(node: any, visitor: Visitor) {
     const operator: string = node.operator;
 
-    if (node.operator !== '&&' && node.operator !== '||')
-      throw Error(`Unsupported logical operator: ${operator}`);
-
     switch (operator) {
       case '&&': {
         const left_side = visitor.visitNode(node.left);
-        return left_side && visitor.visitNode(node.right);
+        if (left_side === true)
+          return visitor.visitNode(node.right);
+        return left_side;
       }
       case '||': {
         const left_side = visitor.visitNode(node.left);
         return left_side || visitor.visitNode(node.right);
       }
+      default:
+        throw Error(`Unsupported logical operator: ${operator}`);
     }
   }
 }
