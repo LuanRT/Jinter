7751d6ef6a9117747a32560e4b1121b1d55adf6c
LuanRT <luan.lrt4@gmail.com>
2023-02-10 07:12:25 -0300

    feat: use ESTree for the AST types

--- src/main.ts
+++ src/main.ts
@@ -1,71 +1,78 @@
 import Visitor from './visitor';
 import { parse } from 'acorn';
-    
+import type { Node } from 'estree';
+
 export default class Jinter {
-  private ast: any;
-  
+  #ast: Node[];
+
   public visitor: Visitor;
   public scope: Map<string, any>;
-  
-  constructor (input: string) {
-    const program = parse(input,  { ecmaVersion: 2020 });
-     
-    this.ast = program.body;
-    
-    this.visitor = new Visitor(this.ast);
+
+  constructor(input: string) {
+    const program = parse(input, { ecmaVersion: 2020 });
+
+    this.#ast = program.body;
+
+    this.visitor = new Visitor(this.#ast);
     this.scope = this.visitor.scope;
-   
-    this.visitor.on('console', (node: any, visitor: Visitor) => {
-      if (node.type === 'Identifier') 
+
+    this.scope.set('print', (args: any[]) => console.log(...args));
+
+    this.visitor.on('console', (node, visitor) => {
+      if (node.type === 'Identifier')
         return console;
-      
-      const prop: keyof Console = visitor.visitNode(node.callee.property);
-      const args = node.arguments.map((arg: any) => visitor.visitNode(arg));
-   
-      const console_prop = console[prop] as Function;
-      
-      if (!console_prop)
-        return 'proceed';
-      
-      return console_prop(...args);
+
+      if (node.type === 'CallExpression' && node.callee.type === 'MemberExpression') {
+        const prop: keyof Console = visitor.visitNode(node.callee.property);
+        const args = node.arguments.map((arg) => visitor.visitNode(arg));
+
+        const console_prop = console[prop] as Function;
+
+        if (!console_prop)
+          return 'proceed';
+
+        return console_prop(...args);
+      } return 'proceed';
     });
-    
-    this.visitor.on('Math', (node: any, visitor: Visitor) => {
-      if (node.type === 'Identifier') 
+
+    this.visitor.on('Math', (node, visitor) => {
+      if (node.type === 'Identifier')
         return Math;
-      
-      const prop: keyof Math = visitor.visitNode(node.callee.property);
-      const args = node.arguments.map((arg: any) => visitor.visitNode(arg));
-   
-      const math_prop = Math[prop] as Function;
-      
-      if (!math_prop)
-        return 'proceed';
-      
-      return math_prop(...(args as [ number, number ]));
+
+      if (node.type === 'CallExpression' && node.callee.type === 'MemberExpression') {
+        const prop: keyof Math = visitor.visitNode(node.callee.property);
+        const args = node.arguments.map((arg) => visitor.visitNode(arg));
+        const math_prop = Math[prop] as Function;
+
+        if (!math_prop)
+          return 'proceed';
+
+        return math_prop(...(args as [number, number]));
+      } return 'proceed';
     });
-    
-    this.visitor.on('String', (node: any, visitor: Visitor) => {
-      if (node.type === 'Identifier') 
+
+    this.visitor.on('String', (node, visitor) => {
+      if (node.type === 'Identifier')
         return String;
-      
-      const prop: keyof typeof String = visitor.visitNode(node.callee.property);
-      const args = node.arguments.map((arg: any) => visitor.visitNode(arg));
-      
-      const string_prop = String[prop] as Function;
-      
-      if (!string_prop)
-        return 'proceed';
-     
-      return string_prop(args);
+
+      if (node.type === 'CallExpression' && node.callee.type === 'MemberExpression') {
+        const prop: keyof typeof String = visitor.visitNode(node.callee.property);
+        const args = node.arguments.map((arg) => visitor.visitNode(arg));
+        const string_prop = String[prop] as Function;
+
+        if (!string_prop)
+          return 'proceed';
+
+        return string_prop(args);
+      } return 'proceed';
     });
-    
-    this.visitor.on('Date', (node: any) => {
-      if (node.type === 'Identifier') 
+
+    this.visitor.on('Date', (node) => {
+      if (node.type === 'Identifier')
         return Date;
     });
   }
-  
+
   public interpret() {
     return this.visitor.run();
   }
--- src/visitor.ts
+++ src/visitor.ts
@@ -1,55 +1,69 @@
 import getNode from './map';
+import type { Node } from 'estree';
+import package_json from '../package.json';
+
+export type Listener = (node: Node, visitor: Visitor) => any;
 
 export default class Visitor {
   public scope: Map<string, any> = new Map();
-  public listeners: { [key: string ]: Function } = {};
- 
-  // TODO: properly type AST, see https://github.com/acornjs/acorn/issues/1136
-  public ast: any[];
-  
-  constructor (ast: any[]) {
+  public listeners: { [key: string]: Listener } = {};
+  public ast: Node[];
+
+  constructor(ast: Node[]) {
     this.ast = ast;
   }
-  
+
   /**
    * Starts interpreting the program.
    */
   public run() {
     let result;
-    
+
     for (const node of this.ast) {
       result = this.visitNode(node);
     }
-    
+
     return result;
   }
-  
+
   /**
    * Visits given node and executes it.
    */
-  public visitNode(node: any) {
+  public visitNode(node?: Node | null) {
     if (!node)
       return null;
 
     try {
       const target_node = getNode(node.type);
       return target_node.visit(node, this);
-    } catch (err: any) {
-      if (err.code === 'MODULE_NOT_FOUND') {
-        console.warn('Node not implemented:', node);
-      } else  {
-        throw err;
-      }
+    } catch (err) {
+      this.#printError(node, err);
     }
   }
-  
-    
+
   /**
-   * Listens for node calls. Can be used to implement new functionality.
-   * @param node_name - the node to listen for.
-   * @param fn - the callback function.
+   * Gets the name of a node.
+   * @param node - The target node.
    */
-  public on(node_name: string, fn: Function) {
-    this.listeners[node_name] = fn;
+  public getName(node: Node) {
+    if (node.type === 'Identifier')
+      return node.name;
+    else if (node.type === 'Literal')
+      return node.value as string;
+  }
+
+  /**
+   * Listens for node calls. Can be used to override default behavior or add new functionality.
+   * @param node_name - The node to listen for.
+   * @param listener - The callback function.
+   */
+  public on(node_name: string, listener: Listener): void {
+    this.listeners[node_name] = listener;
+  }
+
+  #printError(node: Node, err: any) {
+    if (err.code === 'MODULE_NOT_FOUND') {
+      console.warn(`Node ${node.type} not found!\nThis is a bug, please report it at ${package_json.bugs.url}.`);
+    } else throw err;
   }
 }
