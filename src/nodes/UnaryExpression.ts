11867fbd96e17a186d20422a72239272bb2c521e
LuanRT <luan.lrt4@gmail.com>
2023-02-10 07:14:14 -0300

    feat: implement all update, unary, logical and assignment operators

--- src/nodes/UnaryExpression.ts
+++ src/nodes/UnaryExpression.ts
@@ -1,13 +1,26 @@
 import Visitor from '../visitor';
+import type ESTree from 'estree';
 
 export default class UnaryExpression {
-  static visit(node: any, visitor: Visitor) {
+  static visit(node: ESTree.UnaryExpression, visitor: Visitor) {
     const operator = node.operator;
 
     switch (operator) {
       case '-': {
         const arg = visitor.visitNode(node.argument);
         return -arg;
+      }
+      case '+': {
+        const arg = visitor.visitNode(node.argument);
+        return +arg;
+      }
+      case '!': {
+        const arg = visitor.visitNode(node.argument);
+        return !arg;
+      }
+      case '~': {
+        const arg = visitor.visitNode(node.argument);
+        return ~arg;
       }
       case 'void': {
         visitor.visitNode(node.argument);
@@ -17,8 +30,18 @@ export default class UnaryExpression {
         const arg = visitor.visitNode(node.argument);
         return typeof arg;
       }
+      case 'delete': {
+        if (node.argument.type === 'MemberExpression') {
+          const obj = visitor.visitNode(node.argument.object);
+          const prop = node.argument.computed ? visitor.visitNode(node.argument.property) : visitor.getName(node.argument.property);
+          return delete obj[prop];
+        } else if (node.argument.type === 'Identifier') {
+          return visitor.scope.delete(node.argument.name);
+        }
+        return true;
+      }
       default:
-        console.warn('Unsupported unary operator: ', operator);
+        console.warn('Unhandled UnaryExpression operator', operator);
     }
   }
 }
