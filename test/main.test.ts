5c2fbad69a28bb3abd4a7f085235824e69769c54
LuanRT <luan.lrt4@gmail.com>
2023-02-10 07:16:49 -0300

    chore: update examples and tests

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,66 +1,443 @@
 import Jinter from '..';
 
 describe('Jinter Tests', () => { 
-  it('should support the "in" operator', () => {
-    const code = `
-      const obj = { a: 1, b: 2 };
-      const result = 'a' in obj;
-    `;
-    const jinter = new Jinter(code);
-    jinter.interpret();
+  describe('Logical Expressions', () => {
+    it('should support logical AND', () => {
+      const code = `
+        const result = true && false;
+      `;
 
-    const result = jinter.scope.get('result');
-    expect(result).toBeTruthy();
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(false);
+    });
+
+    it('should support logical OR', () => {
+      const code = `
+        const result = true || false;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(true);
+    });
+
+    it('should support nullish coalescing', () => {
+      const code = `
+        const result = null ?? false;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(false);
+    });
   });
 
-  it('should support the "instanceof" operator', () => {
-    const code = `
-      const the_date = new Date('1969-07-20');
-      const result = the_date instanceof Date;
-    `;
+  describe('Update Expressions', () => {
+    it('should support increment', () => {
+      const code = `
+        const num = 10;
+        const result = num++;
+      `;
 
-    const jinter = new Jinter(code);
-    jinter.interpret();
+      const jinter = new Jinter(code);
+      jinter.interpret();
 
-    const result = jinter.scope.get('result');
-    expect(result).toBeTruthy();
+      expect(jinter.scope.get('result')).toEqual(10);
+      expect(jinter.scope.get('num')).toEqual(11);
+    });
+
+    it('should support decrement', () => {
+      const code = `
+        const num = 10;
+        const result = num--;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(10);
+      expect(jinter.scope.get('num')).toEqual(9);
+    });
+
+    it('should support prefix increment', () => {
+      const code = `
+        const num = 10;
+        const result = ++num;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(11);
+      expect(jinter.scope.get('num')).toEqual(11);
+    });
+
+    it('should support prefix decrement', () => {
+      const code = `
+        const num = 10;
+        const result = --num;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(9);
+      expect(jinter.scope.get('num')).toEqual(9);
+    });
   });
 
-  it('should support the "typeof" operator', () => {
-    const code = `
-      const result = typeof 1;
-    `;
+  describe('Unary Expressions', () => {
+    it('should support negation', () => {
+      const code = `
+        const num = 10;
+        const result = -num;
+      `;
 
-    const jinter = new Jinter(code);
-    jinter.interpret();
+      const jinter = new Jinter(code);
+      jinter.interpret();
 
-    const result = jinter.scope.get('result');
-    expect(result).toBe('number');
+      expect(jinter.scope.get('result')).toEqual(-10);
+    });
+
+    it('should support plus', () => {
+      const code = `
+        const num = 10;
+        const result = +num;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(10);
+    });
+
+    it('should support logical not', () => {
+      const code = `
+        const bool = true;
+        const result = !bool;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(false);
+    });
+
+    it('should support bitwise not', () => {
+      const code = `
+        const num = 10;
+        const result = ~num;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(-11);
+    });
+
+    it('should support typeof', () => {
+      const code = `
+        const num = 10;
+        const result = typeof num;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual('number');
+    });
+
+    it('should support void', () => {
+      const code = `
+        const result = void 0;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(undefined);
+    });
+
+    it('should support delete', () => {
+      const code = `
+        const obj = { a: 1, b: 2, c: 3 };
+        const result = delete obj.a;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(true);
+      expect(jinter.scope.get('obj')).toEqual({ b: 2, c: 3 });
+    });
   });
 
-  it('should support the "void" operator', () => {
-    const code = `
-      const result = void 1;
-    `;
+  describe('Binary Expressions', () => {
+    it('should support equality', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = 5;
+        const result = num_1 == num_2;
+      `;
+      const jinter = new Jinter(code);
+      jinter.interpret();
 
-    const jinter = new Jinter(code);
-    jinter.interpret();
+      expect(jinter.scope.get('result')).toEqual(false);
+    });
 
-    const result = jinter.scope.get('result');
-    expect(result).toBeUndefined();
+    it('should support strict equality', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = '10';
+        const result = num_1 === num_2;
+      `;
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(false);
+    });
+
+    it('should support inequality', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = 5;
+        const result = num_1 != num_2;
+      `;
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(true);
+    });
+
+    it('should support strict inequality', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = '10';
+        const result = num_1 !== num_2;
+      `;
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(true);
+    });
+
+    it('should support greater than', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = 5;
+        const result = num_1 > num_2;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(true);
+    });
+
+    it('should support greater than or equal to', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = 5;
+        const result = num_1 >= num_2;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(true);
+    });
+
+    it('should support less than', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = 5;
+        const result = num_1 < num_2;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(false);
+    });
+
+    it('should support addition', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = 5;
+        const result = num_1 + num_2;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(15);
+    });
+
+    it('should support subtraction', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = 5;
+        const result = num_1 - num_2;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(5);
+    });
+
+    it('should support multiplication', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = 5;
+        const result = num_1 * num_2;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(50);
+    });
+
+    it('should support division', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = 5;
+        const result = num_1 / num_2;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(2);
+    });
+
+    it('should support exponentiation', () => {
+      const code = `
+        const num_1 = 2;
+        const num_2 = 3;
+        const result = num_1 ** num_2;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(8);
+    });
+
+    it('should support modulus', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = 3;
+        const result = num_1 % num_2;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(1);
+    });
+
+    it('should support left shift', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = 3;
+        const result = num_1 << num_2;
+      `;
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(80);
+    });
+
+    it('should support right shift', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = 3;
+        const result = num_1 >> num_2;
+      `;
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(1);
+    });
+
+    it('should support unsigned right shift', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = 3;
+        const result = num_1 >>> num_2;
+      `;
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(1);
+    });
+
+    it('should support bitwise AND', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = 3;
+        const result = num_1 & num_2;
+      `;
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(2);
+    });
+
+    it('should support bitwise XOR', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = 3;
+        const result = num_1 ^ num_2;
+      `;
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(9);
+    });
+
+    it('should support bitwise OR', () => {
+      const code = `
+        const num_1 = 10;
+        const num_2 = 3;
+        const result = num_1 | num_2;
+      `;
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toEqual(11);
+    });
+
+    it('should support "in" operator', () => {
+      const code = `
+        const obj = { a: 1, b: 2 };
+        const result = 'a' in obj;
+      `;
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toBeTruthy();
+    });
+
+    it('should support "instanceof" operator', () => {
+      const code = `
+        const that_date = new Date('1969-07-20');
+        const result = that_date instanceof Date;
+      `;
+
+      const jinter = new Jinter(code);
+      jinter.interpret();
+
+      expect(jinter.scope.get('result')).toBeTruthy();
+    });
   });
 
-  it('should support the "^" operator', () => {
-    const code = `
-      const result = 2 ^ 3;
-    `;
-
-    const jinter = new Jinter(code);
-    jinter.interpret();
-
-    const result = jinter.scope.get('result');
-    expect(result).toBe(1);
-  });
 
   it('should define variables', () => {
     const code = `
@@ -124,21 +501,6 @@ describe('Jinter Tests', () => {
     jinter.interpret();
     
     expect(jinter.scope.get('comparison')).toBeFalsy();
-  });
-  
-  it('should do basic math', () => {
-    const code = `
-      const addition = 10 + 5;
-      const subtraction = 200 - 100;
-      const division = 50 / 2;
-    `;
-    
-    const jinter = new Jinter(code);
-    jinter.interpret();
-    
-    expect(jinter.scope.get('addition')).toEqual(15);
-    expect(jinter.scope.get('subtraction')).toEqual(100);
-    expect(jinter.scope.get('division')).toEqual(25);
   });
   
   it('should support for loops', () => {
