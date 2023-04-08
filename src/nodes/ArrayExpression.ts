5a4c8664d0e9441abf96c2eed7bee9bb784ddda4
LuanRT <luan.lrt4@gmail.com>
2023-04-08 18:25:13 -0300

    style: lint

--- src/nodes/ArrayExpression.ts
+++ src/nodes/ArrayExpression.ts
@@ -2,8 +2,6 @@ import type ESTree from 'estree';
 import BaseNode from './BaseJSNode.js';
 
 export default class ArrayExpression extends BaseNode<ESTree.ArrayExpression> {
-  static type = 'ArrayExpression';
-
   public run() {
     return this.node.elements.map((el) => this.visitor.visitNode(el));
   }
