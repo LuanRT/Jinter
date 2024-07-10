328a219f5d8e402e13c0a7ff14cc55131b3f4758
Luan <luan.lrt4@gmail.com>
2024-07-10 00:32:08 -0300

    refactor(main.ts): Simplify logic for defining objects
    And add support for `Array`.

--- src/main.ts
+++ src/main.ts
@@ -23,61 +23,30 @@ export default class Jinter {
 
     this.visitor = new Visitor(this.#ast);
     this.scope = this.visitor.scope;
-
     this.scope.set('print', (args: any[]) => console.log(...args));
 
-    this.visitor.on('console', (node, visitor) => {
+    this.defineObject('console', console);
+    this.defineObject('Math', Math);
+    this.defineObject('String', String);
+    this.defineObject('Array', Array);
+    this.defineObject('Date', Date);
+  }
+
+  public defineObject<T>(name: string, obj: T) {
+    this.visitor.on(name, (node, visitor) => {
       if (node.type === 'Identifier')
-        return console;
+        return obj;
 
       if (node.type === 'CallExpression' && node.callee.type === 'MemberExpression') {
-        const prop: keyof Console = visitor.visitNode(node.callee.property);
+        const prop: keyof T = visitor.visitNode(node.callee.property);
         const args = node.arguments.map((arg) => visitor.visitNode(arg));
+        const callable = obj[prop] as Function | undefined;
 
-        const console_prop = console[prop] as Function;
+        if (!callable)
+          return '__continue_exec';
 
-        if (!console_prop)
-          return 'proceed';
-
-        return console_prop(...args);
-      } return 'proceed';
-    });
-
-    this.visitor.on('Math', (node, visitor) => {
-      if (node.type === 'Identifier')
-        return Math;
-
-      if (node.type === 'CallExpression' && node.callee.type === 'MemberExpression') {
-        const prop: keyof Math = visitor.visitNode(node.callee.property);
-        const args = node.arguments.map((arg) => visitor.visitNode(arg));
-        const math_prop = Math[prop] as Function;
-
-        if (!math_prop)
-          return 'proceed';
-
-        return math_prop(...(args as [number, number]));
-      } return 'proceed';
-    });
-
-    this.visitor.on('String', (node, visitor) => {
-      if (node.type === 'Identifier')
-        return String;
-
-      if (node.type === 'CallExpression' && node.callee.type === 'MemberExpression') {
-        const prop: keyof typeof String = visitor.visitNode(node.callee.property);
-        const args = node.arguments.map((arg) => visitor.visitNode(arg));
-        const string_prop = String[prop] as Function;
-
-        if (!string_prop)
-          return 'proceed';
-
-        return string_prop(args);
-      } return 'proceed';
-    });
-
-    this.visitor.on('Date', (node) => {
-      if (node.type === 'Identifier')
-        return Date;
+        return callable.apply(obj, args);
+      } return '__continue_exec';
     });
   }
 
